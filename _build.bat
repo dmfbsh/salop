9692fc0d8cd1818c29f5afcb87ce420087fa8ac9
dmfbsh <51413860+dmfbsh@users.noreply.github.com>
2023-01-14 12:31:50 +0000

    Updates

--- _build.bat
+++ _build.bat
@@ -1,14 +1,5 @@
 C:
 
 CD C:\Users\David\Documents\OneDrive\Documents\My Documents\GitHub\salop\_ruby
-ruby generate_recents.rb
 
-ruby convert_md_to_yml.rb
-
-REM CD ..\1shropshire\updates
-REM CALL jekyll build --verbose --config _config.yml
-
-COPY _site\1-updateslist.html ..\..\_includes
-
-CD ..\..
-jekyll build --verbose --config _config.yml
+ruby Generate_website.rb
